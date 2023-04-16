7324dc9c563d54cad46ed20006a2c5dc5ec3e46c
nordicgit70 <nordicgit70@users.noreply.github.com>
2023-04-16 10:30:04 +0200

    Return only a LEI record.

--- src/index.ts
+++ src/index.ts
@@ -5,41 +5,23 @@ export type leiRecord = {
     name: string;
     attributes: any;
     relationships: any;
-}
+};
 
-export type gleifResult = {
-    ok: boolean;
-    record: leiRecord | undefined;
-    error? : string;
-}
-
-export async function isin(isin: string): Promise<gleifResult> {
-    let result: gleifResult = { ok: false, record: undefined };
+export async function isin(isin: string): Promise<leiRecord> {
     const url = BASE_URL.concat('/lei-records');
     const params = '?filter[isin]='.concat(isin);
 
-    try {
-        const response = await fetch(url + params);
-        if (response.ok) {
-            const content = await response.json();
-            if (content && Array.isArray(content.data) && content.data.length > 0) {
-                const record = content.data[0];
-                result.ok = true;
-                result.record = {
-                    lei: record.id,
-                    name: record.attributes.entity.legalName.name,
-                    attributes: record.attributes,
-                    relationships: record.relationships
-                }
-
-            } else {
-                result.error = `${isin} not found.`;
-            }
-        }
-
-    } catch(err: any) {
-        result.error = err.message;
-    }
-    console.log(result);
-    return result;
+    const response = await fetch(url + params);
+    if (response.ok) {
+        const content = await response.json();
+        if (content && Array.isArray(content.data) && content.data.length > 0) {
+            const record = content.data[0];
+            return {
+                lei: record.id,
+                name: record.attributes.entity.legalName.name,
+                attributes: record.attributes,
+                relationships: record.relationships,
+            };
+        } else throw new Error(`${isin} not found.`);
+    } else throw new Error('Invalid response: ' + response.statusText);
 }
